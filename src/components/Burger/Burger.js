20e631d7c2a5af4c7675c926843fce6c9679e9f8
Akanksha Shelat <akankshashelat12@gmail.com>
2020-03-03 10:53:13 -0500

    Dynamic Addition of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,28 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = (props) => {
+
+  //the keys are strings
+  let transformedIngredients = Object.keys(props.ingredients)
+    //igKey is the type of ingredient
+    .map((igKey) => {
+      //return an array of X items where X is the value of igKey
+      return[...Array(props.ingredients[igKey])].map( (_, i) => {
+        return <BurgerIngredient key={igKey+i} type={igKey} />
+      });
+    })
+    //we flatten the array
+    .reduce((arr, element) => {
+      return arr.concat(element)
+    }, []);
+
+  if(transformedIngredients.length === 0){
+    transformedIngredients = "Please start adding ingredients."
+  }
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom'/>
     </div>
   );
