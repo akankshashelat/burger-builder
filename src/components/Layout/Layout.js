3f996498aa07c8e767dcf499b8b1eed9ff96f165
Akanksha Shelat <akankshashelat12@gmail.com>
2020-03-12 10:28:52 -0400

    navigation complete with sliding sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,41 @@
-import React, {Fragment} from 'react';
+import React, {Component , Fragment} from 'react';
 import classes from './Layout.css';
 import ToolBar from './../Navigation/Toolbar/Toolbar';
+import SideDrawer from './../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-<Fragment>
-  <div>
-    <ToolBar />
-    SideBar
-  </div>
+class Layout extends Component {
 
-  <main className={classes.Content}>
-    {props.children}
-  </main>
-</Fragment>
-);
+  state = {
+    showSideDrawer: false
+  }
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false
+    })
+  }
 
-export default layout;
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <ToolBar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+          <SideDrawer open={this.state.showSideDrawer} close={this.sideDrawerClosedHandler}/>
+        </div>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Fragment>
+    );
+  }
+
+
+
+}
+
+export default Layout;
